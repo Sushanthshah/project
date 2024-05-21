031e3394d1a78cedc0805b7b0bcbf37905c44c43
Sushanth <sushanthshah23@gmail.com>
2024-05-20 23:55:29 -0500

    added the TaskList package along with controlled and service class

--- src/main/java/com/group2/project/tasklist/package-info.java
+++ src/main/java/com/group2/project/tasklist/package-info.java
@@ -1,1 +1,7 @@
 package com.group2.project.tasklist;
+/* 
+ * 1.Created a entity class called ToDoApp which refers to the database table "todos".
+ * 2. Created the ToDoAppRepository to be able to use the Database.
+ * 3.Created the controller to implement GET, POST, DELETE mapping .
+ * 4.Created the Service class to write the business logic, in this case to use the repository to fetch data from the Database.
+ */
